ef50d9da82cb397ff35f7136b855043d55abb864
Igor <75454521+igorfilgueira@users.noreply.github.com>
2023-11-10 10:31:34 -0300

    Finalização

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,75 +1,18 @@
-import React, {useState} from 'react';
-import {
-	Avatar,
-	Card,
-	CardHeader,
-	Text,
-	ShellBar,
-	ShellBarItem,
-	Icon,
-} from '@ui5/webcomponents-react';
-import {spacing} from '@ui5/webcomponents-react-base';
-import {BarChart, LineChart} from '@ui5/webcomponents-react-charts';
-import lineChartIcon from '@ui5/webcomponents-icons/dist/line-chart.js';
-import barChartIcon from '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
+import React from 'react';
+import {Avatar, ShellBar, ShellBarItem} from '@ui5/webcomponents-react';
 import addIcon from '@ui5/webcomponents-icons/dist/add.js';
-
-const dataset = [
-	{
-		month: 'January',
-		data: 65,
-	},
-	{
-		month: 'February',
-		data: 59,
-	},
-	{
-		month: 'March',
-		data: 80,
-	},
-	{
-		month: 'April',
-		data: 81,
-	},
-	{
-		month: 'May',
-		data: 56,
-	},
-	{
-		month: 'June',
-		data: 55,
-	},
-	{
-		month: 'July',
-		data: 40,
-	},
-];
+import {Navigate, Route, Routes, useNavigate} from 'react-router-dom';
+import {Home} from './Home';
+import {Detail} from './Detail';
 
 export function MyApp() {
-	const [toggleCharts, setToggleCharts] = useState('lineChart');
-	const [loading, setLoading] = useState(false);
-	const handleHeaderClick = () => {
-		if (toggleCharts === 'lineChart') {
-			setLoading(true);
-			setTimeout(() => {
-				setLoading(false);
-				setToggleCharts('barChart');
-			}, 2000);
-		} else {
-			setLoading(true);
-			setTimeout(() => {
-				setLoading(false);
-				setToggleCharts('lineChart');
-			}, 2000);
-		}
+	const navigate = useNavigate();
+	const handleLogoClick = () => {
+		navigate('./');
 	};
 
-	const contentTitle
-    = toggleCharts === 'lineChart' ? 'Line Chart' : 'Bar Chart';
-	const switchToChart
-    = toggleCharts === 'lineChart' ? 'Bar Chart' : 'Line Chart';
 	return (
-		<div>
+		<>
 			<ShellBar
 				logo={<img src='https://github.com/SAPDocuments/Tutorials/raw/master/tutorials/ui5-webcomponents-react-dashboard/reactLogo.png' />}
 				profile={
@@ -78,44 +21,15 @@ export function MyApp() {
 					</Avatar>
 				}
 				primaryTitle='My App'
+				onLogoClick={handleLogoClick}
 			>
 				<ShellBarItem icon={addIcon} text='Add' />
 			</ShellBar>
-			<Card
-				header={
-					<CardHeader
-						titleText='Stock Prices'
-						subtitleText={`Click here to switch to ${switchToChart}`}
-						interactive
-						onClick={handleHeaderClick}
-						avatar={
-							<Icon
-								name={
-									toggleCharts === 'lineChart' ? lineChartIcon : barChartIcon
-								}
-							/>
-						}
-					/>
-				}
-				style={{width: '300px'}}
-			>
-				<Text style={spacing.sapUiContentPadding}>{contentTitle}</Text>
-				{toggleCharts === 'lineChart' ? (
-					<LineChart
-						dimensions={[{accessor: 'month'}]}
-						measures={[{accessor: 'data', label: 'Stock Price'}]}
-						dataset={dataset}
-						loading={loading}
-					/>
-				) : (
-					<BarChart
-						dimensions={[{accessor: 'month'}]}
-						measures={[{accessor: 'data', label: 'Stock Price'}]}
-						dataset={dataset}
-						loading={loading}
-					/>
-				)}
-			</Card>
-		</div>
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/detail' element={<Detail />} />
+				<Route path='/' element={<Navigate replace to='/home' />} />
+			</Routes>
+		</>
 	);
 }
